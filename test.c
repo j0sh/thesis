99c420f82079415e24863a2a56088b3e01d9a9e7
Josh Allmann <joshua.allmann@gmail.com>
2012-04-29 12:47:40 -0700

    Simplify by using opencv operations.

--- test.c
+++ test.c
@@ -3,10 +3,11 @@
 #include <opencv2/imgproc/imgproc_c.h>
 #include "capture.h"
 
-IplImage *avg, *diff, *i32, *bkg, *final;
+IplImage *avg, *diff, *i32, *bkg, *final, *t1, *t2;
 
 static void calc_avgimg(IplImage *img)
 {
+    CvScalar s = cvScalar(1.0, 1.0, 1.0, 0);
     if (!avg) {
         CvSize size = { .width = img->width, .height = img->height };
         avg = cvCreateImage(size, IPL_DEPTH_32F, 3);
@@ -14,6 +15,8 @@ static void calc_avgimg(IplImage *img)
         diff = cvCreateImage(size, IPL_DEPTH_32F, 3);
         bkg = cvCreateImage(size, IPL_DEPTH_32F, 3);
         final = cvCreateImage(size, IPL_DEPTH_32F, 3);
+        t1= cvCreateImage(size, IPL_DEPTH_32F, 3);
+        t2= cvCreateImage(size, IPL_DEPTH_32F, 3);
         cvConvertScale(img, i32, 1/255.0, 0);
         return;
     }
@@ -21,29 +24,12 @@ static void calc_avgimg(IplImage *img)
 
     cvAbsDiff(i32, avg, diff);
     cvRunningAvg(i32, avg, 0.2, NULL);
-
-
-    int i, j, k = 0;
-    char *data = final->imageData;
-    char *diffdata = diff->imageData;
-    char *imgdata = i32->imageData;
-    char *bkgdata = bkg->imageData;
-    for (i = 0; i < img->width; i++) {
-        float *final_p = (float*)(data + k);
-        float *thresh_p = (float*)(diffdata + k);
-        float *img_p = (float*)(imgdata + k);
-        float *bkg_p = (float*)(bkgdata + k);
-        for (j = 0; j < img->height; j++) {
-            float p = *thresh_p++;
-            *final_p++ = (p * *img_p++) + ((1.0 - p) * *bkg_p++);
-            p = *thresh_p++;
-            *final_p++ = (p * *img_p++) + ((1.0 - p) * *bkg_p++);
-            p = *thresh_p++;
-            *final_p++ = (p * *img_p++) + ((1.0 - p) * *bkg_p++);
-
-        }
-        k += img->widthStep*img->nChannels;
-    }
+    
+    cvMul(i32, diff, t1, 1.0);
+    cvConvertScale(diff, diff, -1.0, 0);
+    cvAddS(diff, s, diff, NULL);
+    cvMul(bkg, diff, t2, 1.0);
+    cvAdd(t1, t2, final, NULL);
 }
 
 int main(int argc, char **argv)
@@ -83,6 +69,8 @@ int main(int argc, char **argv)
     cvReleaseImage(&diff);
     cvReleaseImage(&bkg);
     cvReleaseImage(&final);
+    cvReleaseImage(&t1);
+    cvReleaseImage(&t2);
     return 0;
 
  realerr:
