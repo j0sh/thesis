41aad5b79df633a3c274114f7250d73d288b5582
Josh Allmann <joshua.allmann@gmail.com>
2012-08-18 11:29:34 -0700

    capture.c: Better error reporting, update to latest libav.

--- capture.c
+++ capture.c
@@ -1,5 +1,6 @@
 #include "capture.h"
 
+#define ERR(msg) { fprintf(stderr, "%s\n", msg); goto main_error; }
 int start_capture(capture_t *ctx)
 {
     AVFormatContext *ic;
@@ -11,26 +12,27 @@ int start_capture(capture_t *ctx)
     CvSize size;
     av_register_all();
     avdevice_register_all();
-    fmt = av_find_input_format("v4l2");
-    if (!fmt) goto main_error;
+    fmt = av_find_input_format("video4linux2");
+    if (!fmt) ERR("av_find_input_format");
     ic = avformat_alloc_context();
-    if (!ic) goto main_error;
+    if (!ic) ERR("avformat_alloc_context");
     ret = avformat_open_input(&ic, "/dev/video0", fmt, NULL);
-    if (ret < 0) { av_freep(&ic); goto main_error; }
+    if (ret < 0) { av_freep(&ic); ERR("avformat_open_input"); }
     avformat_find_stream_info(ic, NULL);
-    if (ic->nb_streams <= 0) goto main_error; st = ic->streams[0];
-    if (st->codec->codec_type != AVMEDIA_TYPE_VIDEO) goto main_error;
+    if (ic->nb_streams <= 0) ERR("find_stream_info");
+    st = ic->streams[0];
+    if (st->codec->codec_type != AVMEDIA_TYPE_VIDEO) ERR("codec_type");
     sws = sws_getContext(st->codec->width, st->codec->height,
         st->codec->pix_fmt, st->codec->width, st->codec->height,
         PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, 0);
-    if (!sws) goto main_error;
+    if (!sws) ERR("sws_getContext");
     size.width = st->codec->width; size.height = st->codec->height;
     ret = av_image_fill_linesizes(ctx->s_stride, st->codec->pix_fmt, st->codec->width);
-    if (ret < 0) goto main_error;
+    if (ret < 0) ERR("av_image_fill_linesizes");
     ret = av_image_fill_linesizes(ctx->d_stride, PIX_FMT_RGB24, st->codec->width);
-    if (ret < 0) goto main_error;
+    if (ret < 0) ERR("av_image_fill_linesizes 2");
     img = cvCreateImage(size, IPL_DEPTH_8U, 3);
-    if (!img) goto main_error;
+    if (!img) ERR("cvCreateImage");
     av_image_fill_pointers(ctx->img_data, PIX_FMT_RGB24, st->codec->height, (uint8_t*)img->imageData, ctx->d_stride);
 
     ctx->fmt_ctx = ic;
