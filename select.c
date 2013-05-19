72b1653c9cef6d406806231682792c25e83a4d7b
Josh Allmann <joshua.allmann@gmail.com>
2013-05-19 00:32:21 -0700

    select: Add a pointer-based quickselect.
    Much faster swapping -- just two pointers rather than whole
    memory blocks -- at the expense of cache locality.
    
    But faster overall.

--- select.c
+++ select.c
@@ -109,6 +109,79 @@ void pivot_nd(int *a, int sz, int dim, int axis, int p)
     }
 }
 
+static void swap2(int **a, int **b)
+{
+    int *c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int quick_select2(int **arr, int n, int axis)
+{
+    int low, high;
+    int median;
+    int middle, ll, hh;
+
+    low = 0 ; high = n-1 ; median = (low + high) / 2;
+    for (;;) {
+        if (high <= low) /* One element only */
+            return median;
+
+        if (high == low + 1) {  /* Two elements only */
+            if (arr[low][axis] > arr[high][axis])
+                swap2(arr+low, arr+high);
+            return median;
+        }
+
+    /* Find median of low, middle and high items; swap into position low */
+    middle = (low + high) / 2;
+    if (arr[middle][axis] > arr[high][axis]) {
+        swap2(arr+middle, arr+high);
+    }
+    if (arr[low][axis] > arr[high][axis]) {
+        swap2(arr+low, arr+high);
+    }
+    if (arr[middle][axis] > arr[low][axis]) {
+        swap2(arr+middle, arr+low);
+    }
+
+    /* Swap low item (now in position middle) into position (low+1) */
+    swap2(arr+middle, arr+low+1);
+
+    /* Nibble from each end towards middle, swapping items when stuck */
+    ll = low + 1;
+    hh = high;
+    for (;;) {
+        do ll++; while (arr[low][axis] > arr[ll][axis]);
+        do hh--; while (arr[hh][axis]  > arr[low][axis]);
+
+        if (hh < ll)
+        break;
+
+        swap2(arr+ll, arr+hh);
+    }
+
+    /* Swap middle item (in position low) back into correct position */
+    swap2(arr+low, arr+hh);
+
+    /* Re-set active partition */
+    if (hh <= median)
+        low = ll;
+        if (hh >= median)
+        high = hh - 1;
+    }
+}
+
+void pivot2(int **a, int sz, int axis, int p)
+{
+    int i = 0, j = sz - 1;
+    while (1) {
+        while (i < sz && a[i][axis] <= p) i++;
+        while (j >= 0 && a[j][axis] > p) j--;
+        if (j <= i) break;
+        swap2(a+i, a+j);
+    }
+}
 
 #if 0
 static int compare(const void *a, const void *b)
--- select.h
+++ select.h
@@ -6,4 +6,6 @@ int set_swap_buf(int dimensions);
 void free_swap_buf();
 void pivot_nd(int *data, int size, int dimensions, int axis, int p);
 
+int quick_select2(int **data, int size, int axis);
+void pivot2(int **data, int size, int axis, int p);
 #endif
